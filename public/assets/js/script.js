fd0a0fc803b397720329e3f260f1501ec502a881
Matthew Wiser <matt.wiser@icloud.com>
2022-04-23 01:29:08 -0500

    continued work on refining look and functionality of front end, added id attribute to title input to maintain data persistence, continued refinement of event handling

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -37,6 +37,7 @@ function populateNotes(notes) {
         noteEl.append(titleEl);
 
         deleteButtonImgEl.setAttribute("src", "./assets/img/trash.svg");
+        deleteButtonImgEl.setAttribute("class", "delete-button");
 
         deleteButtonLinkEl.setAttribute("href", "#");
         deleteButtonLinkEl.append(deleteButtonImgEl);
@@ -47,16 +48,43 @@ function populateNotes(notes) {
     });
 }
 
+function populateMainNote(note) {
+        
+    const noteTitleEl = document.getElementById("note-title-edit");
+    const noteBodyEl = document.getElementById("note-body");
+    
+    noteTitleEl.value = note.title;
+    noteTitleEl.dataset.id = note.id;
+
+    noteBodyEl.value = note.body;    
+}
+
 function createNewNote() {
     console.log("new note creation request, write POST fetch here");
 }
 
+
+
 document.body.addEventListener('click', event => {
     const eventClass = event.target.className;
-    if (eventClass !== "note-info" && eventClass !== "note-title") {
+    if (eventClass !== "note-info" && eventClass !== "note-title" && eventClass !== "delete-button" && event.target.id !== "save-note") {
       return
     }
-    // define handling for click on note-info/note-title here
+    if (eventClass === "note-info") {
+        populateMainNote(notesArray[event.target.id]);
+    }
+
+    if (eventClass === "note-title") {
+        console.log("note-title clicked!");
+    }
+
+    if (eventClass === "delete-button") {
+        console.log("delete-button clicked!");
+    }
+    
+    if (event.target.id === "save-note") {
+        console.log("save-button clicked!");
+    }
   }  
 )
 addNoteEl.addEventListener('click', createNewNote);
