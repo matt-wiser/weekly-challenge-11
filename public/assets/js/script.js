2efd176ad887484f29076eac65f876096ba91123
Matthew Wiser <matt.wiser@icloud.com>
2022-04-23 00:31:54 -0500

    added basic front end functionality and event handlers for note clicking

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,1 +1,63 @@
-console.log("front end scrript connected");
+const notesArray = [];
+const addNoteEl = document.querySelector('#add-note');
+
+function getNotes () {
+    fetch('/api/notes')
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            else{
+                console.log("ERROR WHEN REQUESTING NOTES");
+            }
+        })
+        .then(notes =>{
+            notes.forEach(note => {
+                notesArray.push(note);
+            });
+            populateNotes(notes);
+        })
+}
+
+function populateNotes(notes) {
+    const noteContainerEl = document.getElementById("notes-container");
+
+    notes.forEach(note => {
+    
+        const noteEl = document.createElement("div");
+        const titleEl = document.createElement("h4");
+        const deleteButtonLinkEl = document.createElement("a");
+        const deleteButtonImgEl = document.createElement("img");
+        
+        noteEl.setAttribute("id", `${note.id}`);
+        noteEl.setAttribute("class", "note-info");
+        
+        titleEl.textContent = note.title;
+        titleEl.setAttribute("class", "note-title");
+        noteEl.append(titleEl);
+
+        deleteButtonImgEl.setAttribute("src", "./assets/img/trash.svg");
+
+        deleteButtonLinkEl.setAttribute("href", "#");
+        deleteButtonLinkEl.append(deleteButtonImgEl);
+
+        noteEl.append(deleteButtonLinkEl);
+
+        noteContainerEl.append(noteEl);
+    });
+}
+
+function createNewNote() {
+    console.log("new note creation request, write POST fetch here");
+}
+
+document.body.addEventListener('click', event => {
+    const eventClass = event.target.className;
+    if (eventClass !== "note-info" && eventClass !== "note-title") {
+      return
+    }
+    // define handling for click on note-info/note-title here
+  }  
+)
+addNoteEl.addEventListener('click', createNewNote);
+getNotes();
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require("path");
+const {notes} = require('./data/db.json');
+
 const PORT = process.env.PORT || 3001
 const app = express();
 
@@ -11,6 +13,10 @@ app.get('/', (req, res) =>{
     res.sendFile(
         path.join(__dirname, './public/index.html')
     )
+});
+
+app.get('/api/notes', (req, res) => {
+    res.send(notes);
 });
 
 app.listen(PORT, () =>{
